17c3e113f96fd7a7fb64de60660412c7efdd5ef8
mr7aali <mr7aali@gmail.com>
2022-12-28 15:06:21 +0600

    d

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -4,6 +4,8 @@ const Footer = () => {
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
           <div className="sm:col-span-2">
+
+            
             <a
               href="/"
               aria-label="Go home"
