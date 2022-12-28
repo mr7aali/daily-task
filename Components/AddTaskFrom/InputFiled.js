49943af9c574d6671094789e5685ecf613d310a2
mr7aali <mr7aali@gmail.com>
2022-12-28 16:00:06 +0600

    d

--- Components/AddTaskFrom/InputFiled.js
+++ Components/AddTaskFrom/InputFiled.js
@@ -14,7 +14,7 @@ export default function InputFiled({ setOpen }) {
         <Box>
             <Box class="min-h-screen py-6 flex flex-col justify-center sm:py-12">
                 <Box class="relative py-3 sm:max-w-xl sm:mx-auto">
-                    <Box class="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
+                    <Box style={{ borderRadius:'15px' }} class="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow  sm:p-10">
                         <Box class="max-w-md mx-auto">
 
 
@@ -34,10 +34,10 @@ export default function InputFiled({ setOpen }) {
 
                             <form onSubmit={handleSubmit(handleModal)}>
                                 <Box class="Boxide-y Boxide-gray-200">
-                                    <Box class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                    <Box sx={{padding:'50px 0'}} class=" text-base leading-6  text-gray-700 sm:text-lg sm:leading-7">
 
 
-                                        <Box class="flex flex-col">
+                                        <Box style={{margin:'30px 0px'}} class="flex flex-col">
                                             {/* <label class="leading-loose">Task Title</label>
                                         <input type="text" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Event title" /> */}
                                             <TextField
@@ -51,7 +51,7 @@ export default function InputFiled({ setOpen }) {
                                         </Box>
 
 
-                                        <Box class="flex flex-col">
+                                        <Box style={{margin:'30px 0px'}} class="flex flex-col">
                                             {/* <label class="leading-loose">Task Subtitle</label>
                                         <input type="text" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Optional" /> */}
                                             <TextField
@@ -63,9 +63,14 @@ export default function InputFiled({ setOpen }) {
                                             />
                                         </Box>
 
-
-                                        <Box class="flex items-center space-x-4">
-                                            <Box class="flex flex-col">
+                                        {/* flex items-center space-x-4 */}
+                                        <Box  sx={{
+                                             display:'flex',
+                                             alignItems:'center',
+                                             flexDirection:'row'
+                                           
+                                        }}    class="">
+                                            <Box  class="flex flex-col">
                                                 <label class="leading-loose">Start</label>
                                                 <Box class="relative focus-within:text-gray-600 text-gray-400">
                                                     <input type="date"  {...register("StartDate")} class="pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="25/02/2020" />
@@ -84,7 +89,7 @@ export default function InputFiled({ setOpen }) {
                                                 </Box>
                                             </Box>
                                         </Box>
-                                        <Box class="flex flex-col">
+                                        <Box style={{margin:'30px 0px'}}  class="flex flex-col">
                                             {/* <label class="leading-loose">Event Description</label>
                                         <input type="text" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Optional" /> */}
                                             <TextField
