ddd72b84c7a8816bd6ea352edab5623c538581b6
mr7aali <mr7aali@gmail.com>
2022-12-28 14:13:19 +0600

    a

--- Components/AddTaskFrom/InputFiled.js
+++ Components/AddTaskFrom/InputFiled.js
@@ -39,6 +39,7 @@ export default function InputFiled({ setOpen }) {
                                             {/* <label class="leading-loose">Task Title</label>
                                         <input type="text" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Event title" /> */}
                                             <TextField
+
                                                 id="outlined-textarea"
                                                 label="Task Title"
                                                 placeholder="Task Title"
@@ -94,11 +95,31 @@ export default function InputFiled({ setOpen }) {
                                             />
                                         </Box>
                                     </Box>
-                                    <Box class="pt-4 flex items-center space-x-4">
-                                        {/* <a  onClick={() => setOpen(false)} class="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none">
-                                            <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg> Cancel
-                                        </a>
-                                        <Button type="submit" class="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">Create</Button> */}
+                                    <Box sx={{
+
+                                        display: 'flex',
+
+                                    }}>
+
+
+
+                                        <Button onClick={() => setOpen(false)} class="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none">
+                                            Cancel
+                                        </Button>
+                                        <Button type="submit" class="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">
+                                            Create
+                                        </Button>
+
+
+
+
+
+
+
+
+
+
+
                                     </Box>
                                 </Box>
                             </form>
